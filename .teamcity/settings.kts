0a5442bbb9cf73711a511bbba64c7de184f905e0
admin <admin@TeamCity>
2020-05-06 19:23:53 +0300

    TeamCity change in 'Issues / TW-65693 Cannot unset a default option in a runner (Rake runner, Kotlin DSL)' project: runners of 'Rake' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -35,15 +35,20 @@ object Rake : BuildType({
     steps {
         step {
             type = "rake-runner"
+            param("ui.rakeRunner.rake.trace.invoke.exec.stages.enabled", "true")
+            param("ui.rakeRunner.frameworks.shoulda.enabled", "true")
+            param("ui.rakeRunner.rake.tasks.names", "hello_world")
+            param("use-custom-build-file", "true")
+            param("ui.rakeRunner.frameworks.testspec.enabled", "true")
             param("build-file", """
                 desc "print hello world!"        # description.
                 task "hello_world" do            # rake task name.
                    p "hello world!!"                 # print "hello world!"
                 end
             """.trimIndent())
-            param("ui.rakeRunner.bunlder.exec.enabled", "false")
-            param("ui.rakeRunner.rake.tasks.names", "hello_world")
-            param("use-custom-build-file", "true")
+            param("ui.rakeRunner.additional.rake.cmd.params", "sss")
+            param("ui.rakeRunner.ruby.use.mode", "path")
+            param("ui.rakeRunner.ruby.interpreter.path", "sss")
         }
     }
 })
